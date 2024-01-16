911ac2d8a8a9141cc20d4254e499709119ccdc31
Anuradhadesilva <anuradhashashimal2@gmail.com>
2024-01-16 14:21:21 +0530

    Initial Commit

---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by Anuradha Desilva on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
