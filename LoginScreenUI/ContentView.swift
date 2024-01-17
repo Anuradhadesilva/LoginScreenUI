89de02046b73df61000bf0ad478977957fcae614
Anuradhadesilva <anuradhashashimal2@gmail.com>
2024-01-17 10:08:20 +0530

    Create login UI and add simple authentication part

--- LoginScreenUI/ContentView.swift
+++ LoginScreenUI/ContentView.swift
@@ -8,16 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongusername = 0
+    @State private var wrongpassword = 0
+    @State private var showingLoginScreen = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ZStack{
+                Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                VStack{
+                    Text("Login:")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Enter username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10.0)
+                        .border(.red, width: CGFloat(wrongusername))
+                    SecureField("Enter password", text:$password)
+                        .padding()
+                        .frame(width: 300,height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10.0)
+                        .border(.red, width: CGFloat(wrongpassword))
+                    Button("Login"){
+                       authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300,height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10.0)
+                    .navigationDestination(isPresented: $showingLoginScreen) {
+                        
+                        Text("Yo have logged @\(username)")
+                        
+                    }
+                }
+            }
+            .navigationBarHidden(true)
         }
-        .padding()
+        
     }
+        func authenticateUser(username: String, password: String){
+            if username.lowercased() == "mario123"{
+                wrongusername = 0
+                if password.lowercased() == "abc123"{
+                    wrongpassword = 0
+                    showingLoginScreen = true
+                }
+                else{
+                    wrongpassword = 2
+                }
+            }else{
+                wrongusername = 2
+            }
+        }
 }
+
+//struct logged : View {
+//    var body: some View {
+//        
+//    }
+//}
 
 #Preview {
     ContentView()
